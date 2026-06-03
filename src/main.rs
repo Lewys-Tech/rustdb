082c755fbdc56a0c9c3db70b22cc18318f3784ad
Lewys-Tech <wislerlewis58@gmail.com>
2026-06-03 14:37:26 +0300

    Added SET GET DELETE & EXIT functions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::io::{self, BufRead, Write};
 use std::collections::HashMap;
 
-let mut store: HashMap<String, String> = HashMap::new();
+
 
 fn main() {
+    let mut store: HashMap<String, String> = HashMap::new();
     let stdin = io::stdin();
 
     loop {
@@ -21,5 +22,40 @@ fn main() {
         let mut parts = line.split_whitespace();
         let cmd = parts.next().unwrap_or("");
         let args: Vec<&str> = parts.collect();
+
+        match cmd {
+            "SET" => {
+                if args.len() < 2 {
+                    println!("Usage: SET <key> <value>");
+                    continue;
+                }
+                let key = args[0].to_string();
+                let value = args[1].to_string();
+                store.insert(key, value);
+                println!("OK");
+            }
+
+            "GET" => {
+                if args.len() < 1 {
+                    println!("Usage: GET <key> ");
+                    continue;
+                }
+                let key = args[0].to_string();
+                match store.get(&key){
+                    Some(value) => println!("{}", value),
+                    None        => println!("(nil)"),
+                }
+                
+                
+            }
+            "DELETE" => {
+                let key = args[0].to_string();
+                store.remove(&key);
+                println!("OK");
+            }
+
+            "EXIT" => std::process::exit(0),
+            _ => println!("Unknown command: {}", cmd),
+        }
     }
 }    
