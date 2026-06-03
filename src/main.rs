c1e4db0990166321899ebdbaba3952f041b51072
Lewys-Tech <wislerlewis58@gmail.com>
2026-06-03 16:27:46 +0300

    Added loading and saving functions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 use std::io::{self, BufRead, Write};
 use std::collections::HashMap;
 
+fn save(store: &HashMap<String, String>){
+    let mut file = std::fs::File::create("db.txt").unwrap();
+    for (key, value) in store.iter() {
+        writeln!(file, "{} {}", key, value).unwrap();
+    }
+}
 
+fn load() -> HashMap<String, String> {
+    let mut store = HashMap::new();
+    if let Ok(content) =  std::fs::read_to_string("db.txt") {
+        for line in content.lines() {
+            let mut parts = line.splitn(2, ' ');
+            let key = parts.next().unwrap_or("").to_string();
+            let value = parts.next().unwrap_or("").to_string();
+            store.insert(key, value);
+        }
+    }
+    store
+}
 
 fn main() {
-    let mut store: HashMap<String, String> = HashMap::new();
+    let mut store = load();
     let stdin = io::stdin();
 
     loop {
@@ -54,7 +72,7 @@ fn main() {
                 println!("OK");
             }
 
-            "EXIT" => std::process::exit(0),
+            "EXIT" => { save(&store); std::process::exit(0);}
             _ => println!("Unknown command: {}", cmd),
         }
     }
